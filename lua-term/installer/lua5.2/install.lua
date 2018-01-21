b435e3b06e34ed3fce9e53d3bf0dcc66af74910b
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2018-01-21 16:30:42 +0100

    Install the LUA files.

--- lua-term/installer/lua5.2/install.lua
+++ lua-term/installer/lua5.2/install.lua
@@ -4,48 +4,78 @@ local tResult
 
 if strDistId=='windows' then
   if strCpuArch=='x86' then
-    t:install('windows_x86/lua_plugins/term/core.dll',         '${install_lua_cpath}/')
+    t:install('windows_x86/lua/term/colors.lua',               '${install_lua_path}/term/')
+    t:install('windows_x86/lua/term/cursor.lua',               '${install_lua_path}/term/')
+    t:install('windows_x86/lua/term/init.lua',                 '${install_lua_path}/term/')
+    t:install('windows_x86/lua_plugins/term/core.dll',         '${install_lua_cpath}/term/')
     tResult = true
   elseif strCpuArch=='x86_64' then
-    t:install('windows_x86_64/lua_plugins/term/core.dll',      '${install_lua_cpath}/')
+    t:install('windows_x86_64/lua/term/colors.lua',            '${install_lua_path}/term/')
+    t:install('windows_x86_64/lua/term/cursor.lua',            '${install_lua_path}/term/')
+    t:install('windows_x86_64/lua/term/init.lua',              '${install_lua_path}/term/')
+    t:install('windows_x86_64/lua_plugins/term/core.dll',      '${install_lua_cpath}/term/')
     tResult = true
   end
 
 elseif strDistId=='ubuntu' then
   if strDistVersion=='14.04' then
     if strCpuArch=='x86' then
-      t:install('ubuntu_1404_x86/lua_plugins/term/core.so',    '${install_lua_cpath}/')
+      t:install('ubuntu_1404_x86/lua/term/colors.lua',         '${install_lua_path}/term/')
+      t:install('ubuntu_1404_x86/lua/term/cursor.lua',         '${install_lua_path}/term/')
+      t:install('ubuntu_1404_x86/lua/term/init.lua',           '${install_lua_path}/term/')
+      t:install('ubuntu_1404_x86/lua_plugins/term/core.so',    '${install_lua_cpath}/term/')
       tResult = true
     elseif strCpuArch=='x86_64' then
-      t:install('ubuntu_1404_x86_64/lua_plugins/term/core.so', '${install_lua_cpath}/')
+      t:install('ubuntu_1404_x86_64/lua/term/colors.lua',      '${install_lua_path}/term/')
+      t:install('ubuntu_1404_x86_64/lua/term/cursor.lua',      '${install_lua_path}/term/')
+      t:install('ubuntu_1404_x86_64/lua/term/init.lua',        '${install_lua_path}/term/')
+      t:install('ubuntu_1404_x86_64/lua_plugins/term/core.so', '${install_lua_cpath}/term/')
       tResult = true
     end
 
   elseif strDistVersion=='16.04' then
     if strCpuArch=='x86' then
-      t:install('ubuntu_1604_x86/lua_plugins/term/core.so',    '${install_lua_cpath}/')
+      t:install('ubuntu_1604_x86/lua/term/colors.lua',         '${install_lua_path}/term/')
+      t:install('ubuntu_1604_x86/lua/term/cursor.lua',         '${install_lua_path}/term/')
+      t:install('ubuntu_1604_x86/lua/term/init.lua',           '${install_lua_path}/term/')
+      t:install('ubuntu_1604_x86/lua_plugins/term/core.so',    '${install_lua_cpath}/term/')
       tResult = true
     elseif strCpuArch=='x86_64' then
-      t:install('ubuntu_1604_x86_64/lua_plugins/term/core.so', '${install_lua_cpath}/')
+      t:install('ubuntu_1604_x86_64/lua/term/colors.lua',      '${install_lua_path}/term/')
+      t:install('ubuntu_1604_x86_64/lua/term/cursor.lua',      '${install_lua_path}/term/')
+      t:install('ubuntu_1604_x86_64/lua/term/init.lua',        '${install_lua_path}/term/')
+      t:install('ubuntu_1604_x86_64/lua_plugins/term/core.so', '${install_lua_cpath}/term/')
       tResult = true
     end
 
   elseif strDistVersion=='17.04' then
     if strCpuArch=='x86' then
-      t:install('ubuntu_1704_x86/lua_plugins/term/core.so',    '${install_lua_cpath}/')
+      t:install('ubuntu_1704_x86/lua/term/colors.lua',         '${install_lua_path}/term/')
+      t:install('ubuntu_1704_x86/lua/term/cursor.lua',         '${install_lua_path}/term/')
+      t:install('ubuntu_1704_x86/lua/term/init.lua',           '${install_lua_path}/term/')
+      t:install('ubuntu_1704_x86/lua_plugins/term/core.so',    '${install_lua_cpath}/term/')
       tResult = true
     elseif strCpuArch=='x86_64' then
-      t:install('ubuntu_1704_x86_64/lua_plugins/term/core.so', '${install_lua_cpath}/')
+      t:install('ubuntu_1704_x86_64/lua/term/colors.lua',      '${install_lua_path}/term/')
+      t:install('ubuntu_1704_x86_64/lua/term/cursor.lua',      '${install_lua_path}/term/')
+      t:install('ubuntu_1704_x86_64/lua/term/init.lua',        '${install_lua_path}/term/')
+      t:install('ubuntu_1704_x86_64/lua_plugins/term/core.so', '${install_lua_cpath}/term/')
       tResult = true
     end
   end
 
   elseif strDistVersion=='17.10' then
     if strCpuArch=='x86' then
-      t:install('ubuntu_1710_x86/lua_plugins/term/core.so',    '${install_lua_cpath}/')
+      t:install('ubuntu_1710_x86/lua/term/colors.lua',         '${install_lua_path}/term/')
+      t:install('ubuntu_1710_x86/lua/term/cursor.lua',         '${install_lua_path}/term/')
+      t:install('ubuntu_1710_x86/lua/term/init.lua',           '${install_lua_path}/term/')
+      t:install('ubuntu_1710_x86/lua_plugins/term/core.so',    '${install_lua_cpath}/term/')
       tResult = true
     elseif strCpuArch=='x86_64' then
-      t:install('ubuntu_1710_x86_64/lua_plugins/term/core.so', '${install_lua_cpath}/')
+      t:install('ubuntu_1710_x86_64/lua/term/colors.lua',      '${install_lua_path}/term/')
+      t:install('ubuntu_1710_x86_64/lua/term/cursor.lua',      '${install_lua_path}/term/')
+      t:install('ubuntu_1710_x86_64/lua/term/init.lua',        '${install_lua_path}/term/')
+      t:install('ubuntu_1710_x86_64/lua_plugins/term/core.so', '${install_lua_cpath}/term/')
       tResult = true
     end
   end
